54cc0a41d72d333b0bf8ebbfb5098c9b0f8c1986
heemalsyangbo <heemalsyanbgo123@gmail.com>
2025-01-14 10:35:20 -0500

    add funtion  user input and type conversion

--- labs/week2/lab2.py
+++ labs/week2/lab2.py
@@ -4,7 +4,14 @@ print("elements: ", elements)
 # def funct_name():
 #     return True
 
-def say_greeting(name, message):
-    print(f"{message}, {name}")
-
-say_greeting("Heemal")
+# def say_greeting(name, message):
+#     print(f"{message}, {name}")
+#
+# say_greeting("Heemal")
+def get_valid_int_input(prompt):
+    while True:
+        try:
+            return int(input(print))
+        except ValueError:
+            print("Error: Please enter a valid interger!")
+            continue
