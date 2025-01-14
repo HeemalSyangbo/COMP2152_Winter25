6ddddf3a950442e8ebf86b4ba0170602d1cdca48
heemalsyangbo <heemalsyanbgo123@gmail.com>
2025-01-14 10:41:11 -0500

    add funtion

--- labs/week2/lab2.py
+++ labs/week2/lab2.py
@@ -4,14 +4,14 @@ print("elements: ", elements)
 # def funct_name():
 #     return True
 
-# def say_greeting(name, message):
-#     print(f"{message}, {name}")
-#
+# def say_greeting(name, message="hi"):
+#     print(f" {message}, {name}")
 # say_greeting("Heemal")
+# say_greeting("Heemal", "Hello"
 def get_valid_int_input(prompt):
     while True:
         try:
             return int(input(print))
         except ValueError:
-            print("Error: Please enter a valid interger!")
+            print("Error: Please enter a valid integer!")
             continue
