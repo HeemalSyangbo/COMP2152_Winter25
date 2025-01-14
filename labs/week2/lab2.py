96eeb397e00d1ffefd7600854f1e2082eb2a1250
heemalsyangbo <heemalsyanbgo123@gmail.com>
2025-01-14 10:29:22 -0500

    add funtions

--- labs/week2/lab2.py
+++ labs/week2/lab2.py
@@ -1,2 +1,10 @@
 elements = ["Hydrogen", "Helium", "Lithium", "Beryllium", "Boron", "Carbon"]
 print("elements: ", elements)
+
+# def funct_name():
+#     return True
+
+def say_greeting(name, message):
+    print(f"{message}, {name}")
+
+say_greeting("Heemal")
